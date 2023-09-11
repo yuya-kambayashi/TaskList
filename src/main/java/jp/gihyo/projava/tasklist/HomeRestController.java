8fad4d7b14184134224241bd50b09427410dbbfe
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-11 16:36:31 +0900

    add restController

---
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -0,0 +1,19 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+public class HomeRestController {
+    @RequestMapping(value="/resthello")
+    String hello(){
+        return """
+                Hello, 
+                It works!
+                now is %s.
+                """.formatted(LocalDateTime.now());
+    }
+}
