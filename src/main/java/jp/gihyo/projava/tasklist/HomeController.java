df4d72919678462585d1f54e56eb36c8da7bdee2
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-12 10:58:53 +0900

    add home

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -2,16 +2,28 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    record TaskItem(String id, String task, String deadline, boolean done){}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping(value="/hello")
     String hello(Model model){
         model.addAttribute("time", LocalDateTime.now());
 
         return "hello";
+    }
+
+    @GetMapping("/list")
+    String listItems(Model model){
+        model.addAttribute("tasklist", taskItems);
+        return "home";
     }
 }
