812f128fad1e46092f57384f4ee9cdebe5319b2a
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-12 09:49:48 +0900

    add addItem

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,12 +1,21 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
+
+    record TaskItem(String id, String task, String deadline, boolean done){}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping(value="/resthello")
     String hello(){
         return """
@@ -14,5 +23,14 @@ public class HomeRestController {
                 It works!
                 now is %s.
                 """.formatted(LocalDateTime.now());
+    }
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "added task.";
     }
 }
