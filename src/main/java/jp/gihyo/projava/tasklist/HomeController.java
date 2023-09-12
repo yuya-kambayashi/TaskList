34c3c6fc1e13635f193bcc2d5061216cf491f8ea
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-12 11:00:45 +0900

    add addItem

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -25,5 +27,14 @@ public class HomeController {
     String listItems(Model model){
         model.addAttribute("tasklist", taskItems);
         return "home";
+    }
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "redirect:/list";
     }
 }
