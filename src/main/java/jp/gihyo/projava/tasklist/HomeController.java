9c933eb61ac40043d0915bbfb80bb573e86a3b19
yuya-kambayashi <kambayashi73@gmail.com>
2023-09-12 10:40:41 +0900

    add Thymeleaf

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,27 +1,17 @@
 package jp.gihyo.projava.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/hello")
-    @ResponseBody
-    String hello(){
-        return """
-                <html>
-                    <head>
-                        <title>Hello</head>
-                    </title>
-                    <body>
-                        <h1>Hello</h1>
-                        It works<br>
-                        now is %s.
-                    </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+    @RequestMapping(value="/hello")
+    String hello(Model model){
+        model.addAttribute("time", LocalDateTime.now());
+
+        return "hello";
     }
 }
